5c79de6f18d7d8fc1f5bcbdb1d3050926644afa4
Min Hee  Park <minheekangg@gmail.com>
2018-10-22 13:53:55 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,15 +1,7 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   def change
-    def self.create_table
-      sql = <<-SQL
-      CREATE TABLE students (
-        id INTEGER PRIMARY KEY,
-        name STRING
-      )
-      SQL
-      DB[:conn].execute(sql)
-      end
-
+    create_table :students do |t|
+      t.string :name
   end
 
 end
